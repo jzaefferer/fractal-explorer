85ff9c43040c7fbd8ab2d71b1ad10c8fd66a5cb8
Jörn Zaefferer <joern.zaefferer@gmail.com>
2020-09-22 15:58:27 +0200

    cleanup

--- shadertoy-sketch.js
+++ shadertoy-sketch.js
@@ -1,14 +1,7 @@
-// Click within the image to toggle
-// the shader used by the quad shape
-// Note: for an alternative approach to the same example,
-// involving changing uniforms please refer to:
-// https://p5js.org/reference/#/p5.Shader/setUniform
-
 let starnest;
 let snow;
 let mandelbrot;
 let active;
-let time = 0.0;
 
 function preload() {
   // note that we are using two instances
@@ -33,12 +26,8 @@ function setup() {
 }
 
 function draw() {
-  // update the offset values for each shader,
-  // moving orangeBlue in vertical and redGreen
-  // in horizontal direction
-  time += 0.01;
   active.setUniform("iResolution", [window.innerWidth, window.innerHeight]);
-  active.setUniform("iTime", [time]);
+  active.setUniform("iTime", frameCount / 50);
   active.setUniform("iGlobalTime", millis() / 1000.0);
   active.setUniform("iMouse", [
     mouseX,
